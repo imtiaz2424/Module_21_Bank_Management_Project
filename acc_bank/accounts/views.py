4fdbe19d5a9cb8b5f4162616069de00c1b4d355e
Md Imtiaz Sharif <imtiaz2424@gmail.com>
2024-06-09 19:33:41 +0600

    uncomplete login, profile

--- acc_bank/accounts/views.py
+++ acc_bank/accounts/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import UserRegistrationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 
 class UserRegistrationView(FormView):
-    template_name = ''
+    template_name = 'accounts/user_registration.html'
     form_class = UserRegistrationForm
-    success_url = ''
+    success_url = reverse_lazy('register')
 
-    def form_valid(self, form):
+    def form_valid(self, form):        
         user = form.save()
-        login(user)        
+        login(self.request, user)        
         return super().form_valid(form) # form_valid function call hobe jodi sob thik thake
     
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/user_login.html'
+    def get_success_url(self):
+        return reverse_lazy('home')
     
+
+class UserLogoutView(LogoutView):    
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy('home')
