ea5a298fcda6cc66e03c9f75b76d35c30aeaf69b
Md Imtiaz Sharif <imtiaz2424@gmail.com>
2024-06-09 07:56:45 +0600

    first commit

---
+++ acc_bank/accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import FormView
+from .forms import UserRegistrationForm
+from django.contrib.auth import login
+
+# Create your views here.
+
+class UserRegistrationView(FormView):
+    template_name = ''
+    form_class = UserRegistrationForm
+    success_url = ''
+
+    def form_valid(self, form):
+        user = form.save()
+        login(user)        
+        return super().form_valid(form) # form_valid function call hobe jodi sob thik thake
+    
+    
